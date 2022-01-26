3604409282dabdb7d6caf7677a0b8e24ec63cfc8
ryanmcfadden27 <97765028+ryanmcfadden27@users.noreply.github.com>
2022-01-26 16:03:50 -0600

    Basic Math Shell

--- shell.py
+++ shell.py
@@ -1,2 +1,18 @@
 #shell for practice-repository-shell
 
+from tempfile import TemporaryFile
+
+
+while 1:
+    x = input(">>>")
+    if x == 'exit':
+        break
+
+    try:
+        y = eval(x)
+        if y: print(y)
+    except:
+        try:
+            exec(x)
+        except Exception as e:
+            print("error:", e)
